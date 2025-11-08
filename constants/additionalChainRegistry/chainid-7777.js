61cb992fdbd30a90e8a810400e07c3f8d9bc5ceb
Sean Tichenor <jtart2133014@gmail.com>
2025-11-08 13:27:11 -0500

    Create chainid-7777.js

---
+++ constants/additionalChainRegistry/chainid-7777.js
@@ -0,0 +1,18 @@
+{
+  "name": "XemChain",
+  "chainId": 7777,
+  "networkId": 7777,
+  "rpc": ["https://rpc.tgdk.io"],
+  "nativeCurrency": {
+    "name": "Ytherium",
+    "symbol": "YTH",
+    "decimals": 18
+  },
+  "explorers": [
+    {
+      "name": "XemChain Explorer",
+      "url": "https://explorer.tgdk.io",
+      "standard": "EIP3091"
+    }
+  ]
+}
